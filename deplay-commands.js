45294dfe51430cc572b01dfc9ecff628f78472cd
etgvkghyrg <115227053+etgvkghyrg@users.noreply.github.com>
2023-05-14 16:44:20 +0900

    Update deplay-commands.js

--- deplay-commands.js
+++ deplay-commands.js
@@ -1,1 +1,20 @@
+// deplay-commands.js
+const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { token, clientId, guildId } = require('./config.json');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command);
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then(() => console.log('Successfully registered application commands.'))
+  .catch(console.error);
 
